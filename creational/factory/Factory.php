6d79886209be28108e0debaba1a5086cd4a7b474
kalimoldayev02 <kalimoldayev02@gmail.com>
2023-07-07 15:20:46 +0600

    паттерны

---
+++ creational/factory/Factory.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Creational\Factory;
+
+/*
+ * фабрика состоит из создателей (это сами фабрики) и продукты (объекты которые создают фабрики)
+ * */
+
+interface Transport // интерфейс для продуктов/подклассов
+{
+    public function load(): void;
+
+    public function deliver(string $cargo): void;
+
+    public function unload(): void;
+}
+
+class Plane implements Transport // продукт
+{
+
+    public function load(): void
+    {
+        echo "The plane is loading... \n";
+    }
+
+    public function deliver(string $cargo): void
+    {
+        echo "The plane delivering $cargo \n";
+        }
+
+    public function unload(): void
+    {
+        echo "The plane is unloading...\n";
+    }
+}
+
+class Truck implements Transport // продукт
+{
+
+    public function load(): void
+    {
+        echo "The truck is loading... \n";
+    }
+
+    public function deliver(string $cargo): void
+    {
+        echo "The truck delivering $cargo \n";
+    }
+
+    public function unload(): void
+    {
+        echo "The truck is unloading...\n";
+    }
+}
+
+abstract class Logistic
+{
+    abstract public function getTransport(): Transport;
+
+    public function deliverCargo(string $cargo): void
+    {
+        $transport = $this->getTransport(); // возвращает Plane/Truck
+
+        $transport->load();
+        $transport->deliver($cargo);
+        $transport->unload();
+    }
+}
+
+class AirLogistic extends Logistic // фабрика
+{
+    public function getTransport(): Transport
+    {
+        return new Plane();
+    }
+}
+
+class RoadLogistic extends Logistic // фабрика
+{
+    public function getTransport(): Transport
+    {
+        return new Truck();
+    }
+}
+
+function deliver(Logistic  $logistic, string $cargo)
+{
+    $logistic->deliverCargo($cargo);
+}
+
+
+// вывод
+deliver(new AirLogistic(), "bike");
+echo PHP_EOL;
+deliver(new RoadLogistic(), "bike");
---
+++ creational/singleton/Singleton.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Creational\Singleton;
+
+class Singleton
+{
+    private static ?self $instance = null;
+
+    protected function __construct() { }  // защищаем от создания через new Singleton
+
+    protected function __clone() { } // защищаем от создания через клонирование
+
+    public static function getInstance(): static
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+}
+
+// вывод
+$pattern1 = Singleton::getInstance();
+$pattern2 = Singleton::getInstance();
+
+if ($pattern1 === $pattern2) {
+    echo "1" . PHP_EOL;
+    return;
+}
+echo "0" . PHP_EOL;
